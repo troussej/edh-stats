8945c9da5b49091478b2b95226ae7e00f31a884a
Joël Trousset <trousset.joel@gmail.com>
2025-11-09 22:07:39 +0100

    init : primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), provideClientHydration(withEventReplay())
---
+++ src/app/components/test/test.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-test',
+  imports: [ButtonModule],
+  templateUrl: './test.html',
+  styleUrl: './test.scss',
+})
+export class Test {
+
+}
