3b80d3b58db315d9cd7d3ce9ddf8ee632d653f21
Joël Trousset <trousset.joel@gmail.com>
2026-08-08 14:55:41 +0200

    css + grid

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Dashboard } from './components/dashboard/dashboard';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Dashboard],
+  imports: [RouterOutlet, Dashboard, MenubarModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
