238bbebe777915b470618241521149575f6ba8ed
Joël Trousset <trousset.joel@gmail.com>
2026-08-31 13:02:42 +0200

    feat: filter by bracket

--- src/app/pages/commanders/commanders.ts
+++ src/app/pages/commanders/commanders.ts
@@ -1,7 +1,7 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, model } from '@angular/core';
 import { StatsService } from 'app/services/stats.service';
 import { TableModule } from "primeng/table";
-import _ from 'lodash';
+import _, { values } from 'lodash';
 import { AvatarModule } from "primeng/avatar";
 import { FormsModule } from '@angular/forms';
 import { ConfigService } from 'app/services/config.service';
@@ -9,13 +9,14 @@ import { CardModule } from "primeng/card";
 import { InputTextModule } from 'primeng/inputtext';
 import { TagModule } from 'primeng/tag';
 import { CommanderCard } from 'app/components/commander-card/commander-card';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 
 
 
 @Component({
   selector: 'app-commanders',
-  imports: [TableModule, AvatarModule, FormsModule, CardModule, InputTextModule, TagModule, CommanderCard],
+  imports: [TableModule, AvatarModule, FormsModule, CardModule, InputTextModule, TagModule, CommanderCard, SelectButtonModule],
   templateUrl: './commanders.html',
   styleUrl: './commanders.css',
 })
@@ -26,9 +27,34 @@ export class Commanders {
   public statsService = inject(StatsService);
   public config = inject(ConfigService).config;
 
+  public brackets = [
+    {
+      name: 'B2',
+      value: '2'
+    },
+    {
+      name: 'B3',
+      value: '3'
+    },
+    {
+      name: 'B3+',
+      value: '3+'
+    },
+    {
+      name: 'B4',
+      value: '4'
+    },
+
+  ]
+  public bracketFilter = model<string[]>(['2', '3', '3+', '4',]);
+
 
   public commanders = computed(() => {
-    return _.chain(this.statsService.commanders()).values().filter(c => c.commander.indexOf(this.filter) >= 0).value();
+    return _.chain(this.statsService.commanders()).values()
+      .filter(c => c.commander.indexOf(this.filter) >= 0)
+      .filter(c => this.bracketFilter().includes(c.bracket)
+      )
+      .value();
   }
   );
 
